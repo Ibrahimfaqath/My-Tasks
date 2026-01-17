b31d9e42a1041195fedd2cdf738a81d5e450a46b
Ibrahim <ibrahimfaqath@gmail.com>
2026-01-17 09:52:43 +0700

    delete folder angular

---
+++ index.js
@@ -0,0 +1,187 @@
+// index.js
+import { serveStatic } from "@hono/node-server/serve-static";
+
+import "dotenv/config";
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { db } from "./db/index.js";
+import { users, todos } from "./db/schema.js";
+import bcrypt, { decodeBase64 } from "bcryptjs";
+
+import jwt from "jsonwebtoken";
+import { setCookie } from "hono/cookie";
+
+import { getCookie } from "hono/cookie";
+
+import { and, eq } from "drizzle-orm";
+// import { createContext } from "react";
+
+const app = new Hono();
+
+// API Registrasi
+app.post("api/register", async (c) => {
+  try {
+    const { username, password } = await c.req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await db
+      .insert(users)
+      .values({ username, password: hashedPassword })
+      .returning({ id: users.id, username: users.username });
+
+    return c.json({ success: true, data: newUser[0] }, 201);
+  } catch (error) {
+    return c.json({ success: false, message: "Register gagal" }, 400);
+  }
+});
+
+// API LOGIN
+app.post("/api/login", async (c) => {
+  const { username, password } = await c.req.json();
+  const user = await db.query.users.findFirst({
+    where: (users, { eq }) => eq(users.username, username),
+  });
+
+  if (!user)
+    return c.json(
+      { success: false, message: "Username atau password salah" },
+      401
+    );
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid)
+    return c.json(
+      { success: false, message: "Username atau password salah " },
+      401
+    );
+
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  setCookie(c, "token", token, {
+    httpOnly: true,
+    sameSite: "Lax",
+    maxAge: 3600,
+  });
+
+  return c.json({ success: true, message: "Login berhasil" });
+});
+
+// API LOGOUT
+app.post("/api/logout", (c) => {
+  setCookie(c, "token", "", { maxAge: -1 });
+  return c.json({ success: true, message: "Logout berhasil" });
+});
+
+// API GET ME
+app.get("/api/me", (c) => {
+  const token = getCookie(c, "token");
+  if (!token) return c.json({ success: false, message: "Unauthorized" }, 401);
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+
+    return c.json({ success: true, data: user });
+  } catch (error) {
+    return c.json({ success: false, message: "Unauthorized" }, 401);
+  }
+});
+
+// API Menambah Todo
+app.post("/api/todos", async (c) => {
+  const token = getCookie(c, "token");
+  if (!token) return c.json({ success: false, message: "Unauthorized" }, 401);
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    const { note } = await c.req.json();
+    const newTodo = await db
+      .insert(todos)
+      .values({ note, userId: user.id })
+      .returning();
+    return c.json({ success: true, data: newTodo[0] }, 201);
+  } catch (error) {
+    return c.json({ success: false, message: "Unauthorized" }, 401);
+  }
+});
+
+// API Melihat Semua Todo Milik User
+app.get("/api/todos", async (c) => {
+  const token = getCookie(c, "token");
+  if (!token) return c.json({ success: false, message: "Unauhorized" }, 401);
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    const userTodos = await db.query.todos.findMany({
+      where: (todos, { eq }) => eq(todos.userId, user.id),
+    });
+    return c.json({ success: true, data: userTodos });
+  } catch (error) {
+    return c.json({ success: false, message: "Unauthorized" }, 401);
+  }
+});
+
+// API Edit Todos
+app.put("/api/todos/:id/status", async (c) => {
+  // Dapatkan token dari cookie
+  const token = getCookie(c, "token");
+  if (!token) return c.json({ success: false, message: "Unauthorized" }, 401);
+
+  try {
+    // Verifikasi user dari token
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    const id = parseInt(c.req.param("id"));
+    const { status } = await c.req.json();
+
+    const updatedTodo = await db
+      .update(todos)
+      .set({ status })
+      .where(and(eq(todos.id, id), eq(todos.userId, user.id)))
+      .returning();
+
+    if (updatedTodo.length === 0)
+      return c.json({ success: false, message: "Todo not found" }, 404);
+    return c.json({ success: true, data: updatedTodo[0] });
+  } catch (error) {
+    return c.json({ success: "false", message: "Unauthorized" }, 401);
+  }
+});
+
+// API delete todo
+app.delete("/api/todos/:id", async (c) => {
+  // Dapatkan token dari cookie
+  const token = getCookie(c, "token");
+  if (!token) return c.json({ success: false, message: "Unauthorized" }, 401);
+
+  try {
+    // verifikasi user dari token
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    const id = parseInt(c.req.param("id"));
+    // const { status } = await c.req.json();
+
+    const deletedTodo = await db
+      .delete(todos)
+      .where(and(eq(todos.id, id), eq(todos.userId, user.id)))
+      .returning();
+
+    if (deletedTodo.length === 0)
+      return c.json({ success: false, message: "Todo not found" }, 404);
+    return c.json({ success: true, message: "Todo deleted" });
+  } catch (error) {
+    return c.json({ success: "false", message: "Unauthorized" }, 401);
+  }
+});
+
+// Serve Static
+app.use("/*", serveStatic({ root: "./public" }));
+
+if (process.env.VERCEL) {
+  // Jika berjalan di Vercel, ekspor aplikasi Hono
+  // Vercel akan menangani servernya
+  console.log("Running on Vercel");
+  globalThis.app = app; // Pastikan app dapat diakses secara global oleh Vercel
+} else {
+  // Jika berjalan di lokal, jalankan server development
+  const port = 5000;
+  console.log(`✅ Server is running on http://localhost:${port}`);
+  serve({ fetch: app.fetch, port });
+}
